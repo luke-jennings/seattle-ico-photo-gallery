3f60f64ebfce13889535755e8f2bcb9743ecbd8c
Luke Jennings <lukeajennings@protonmail.com>
2019-04-01 21:45:58 -0700

    Added two Filter tests and updated manifest & index with Seattle ICO name.

--- src/Filter.spec.tsx
+++ src/Filter.spec.tsx
@@ -5,30 +5,54 @@ import { IFilterProps } from './interfaces/IFilterProps';
 
 describe("The photos filter", () => {
 
-    it("When clicked the filter submit button should call the onSubmit function.", () => {
+    // Arrange
+    const onTripTypeChangeHandler = jest.fn();
+    const onTeamsChangeHandler = jest.fn();
+    const onSubmitHandler = jest.fn();
 
-        const submitHandler = jest.fn();
+    const filterProps: IFilterProps = {
+        values: { tripTypeId: 0, teamId: 0 },
+        options: { tripTypeOptions: [], teamOptions: [] },
+        onSubmit: onSubmitHandler,
+        onTripTypeChange: onTripTypeChangeHandler,
+        onTeamChange: onTeamsChangeHandler 
+    }
 
-        const filterProps: IFilterProps = {
-            values: { tripTypeId: 0, teamId: 0 },
-            options: { tripTypeOptions: [], teamOptions: [] },
-            onSubmit: submitHandler,
-            onTripTypeChange: jest.fn(),
-            onTeamChange: jest.fn() 
-        }
+    const { getByText, getByLabelText } = render(
+                                    <Filter
+                                    values={ filterProps.values }
+                                    options={ filterProps.options }
+                                    onSubmit={ filterProps.onSubmit }
+                                    onTripTypeChange={ filterProps.onTripTypeChange}
+                                    onTeamChange={filterProps.onTeamChange}
+                                    />
+                                );
 
-        const { getByText } = render(
-                                        <Filter
-                                        values={ filterProps.values }
-                                        options={ filterProps.options }
-                                        onSubmit={ filterProps.onSubmit }
-                                        onTripTypeChange={ filterProps.onTripTypeChange}
-                                        onTeamChange={filterProps.onTeamChange}
-                                        />
-                                    );
+    it("When changed, the trip types select component should call the onTripTypeChange handler.", () => {
 
+        // Act
+        fireEvent.change(getByLabelText('Trip Type'))
+
+        // Assert
+        expect(onTripTypeChangeHandler).toHaveBeenCalledTimes(1);
+    });
+
+    it("When changed, the teams select component should call the onTeamChange handler.", () => {
+
+        // Act
+        fireEvent.change(getByLabelText('Teams'))
+
+        // Assert
+        expect(onTeamsChangeHandler).toHaveBeenCalledTimes(1);
+    });
+
+    it("When clicked, the filter submit button should call the onSubmit onSubmit handler.", () => {
+
+        // Act
         fireEvent.click(getByText('Search'))
-        expect(submitHandler).toHaveBeenCalledTimes(1);
+
+        // Assert
+        expect(onSubmitHandler).toHaveBeenCalledTimes(1);
     });
 
 })
--- src/Select.tsx
+++ src/Select.tsx
@@ -18,8 +18,8 @@ class Select extends React.Component<ISelectProps> {
     public render() {
         return (
             <>
-                <label htmlFor={this.props.selectName} className="small">{this.props.labelText}:</label>
-                <select name={this.props.selectName} className="form-control" value={this.props.selectedValue} onChange={this.props.onChange}>
+                <label htmlFor={this.props.selectName} className="small">{this.props.labelText}</label>
+                <select id={this.props.selectName} name={this.props.selectName} className="form-control" value={this.props.selectedValue} onChange={this.props.onChange}>
                     {this.props.options.map(o => (
                         <option key={o.value} value={o.value}>{o.text}</option>
                     ))}
