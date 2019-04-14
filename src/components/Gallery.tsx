79040840c58c2bcda04c3f731ee8d044cc55db06
Luke Jennings <lukeajennings@protonmail.com>
2019-04-13 17:09:40 -0700

    Added component FilterMessage and PhotoGrid to simplify Gallery component.

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,6 +1,7 @@
 import React, { ChangeEvent } from 'react';
 import Filter from './Filter'
-import Photo from './Photo';
+import PhotoGrid from './PhotoGrid';
+import FilterMessage from './FilterMessage';
 
 import ReactPaginate from 'react-paginate';
 
@@ -10,37 +11,30 @@ import { Data } from '../services/Data';
 import { ISelectOption } from '../interfaces/ISelectOption';
 import { IPhoto } from '../interfaces/IPhoto';
 
-interface IGalleryProps {
-}
-
 interface IGalleryState extends IFilterValues {
     isLoading: boolean;
-    photoGrid: number[][];
     pageCount: number;
     selectedPage: number;
+    photos: IPhoto[];
 }
 
-class Gallery extends React.Component<IGalleryProps, IGalleryState> {
+class Gallery extends React.Component<{}, IGalleryState> {
 
     filterOptions: IFilterOptions;
+    // filterOptionSelected is a property that duplicates the IFilterValues in state, but this is done so that updating the state of a
+    // filter does not automatically update the filter message (which should not be updated until the user clicks the Search button)
     filterOptionsSelected: IFilterValues;
-    photos: IPhoto[];
-    columns: number;
-    rows: number;
-    pageSize: number;
+    
+    readonly pageSize: number = 12;
 
-    constructor(props: IGalleryProps) {
+    constructor(props: {}) {
         super(props);
-        this.state = {tripType: { value: 17, text: 'Whitewater Rafting' }, team: { value: 24, text: 'South Shore' }, isLoading: true, photoGrid: [], pageCount: 0, selectedPage: 0 };
+        this.state = {tripType: { value: 17, text: 'Whitewater Rafting' }, team: { value: 24, text: 'South Shore' }, isLoading: true, pageCount: 0, selectedPage: 0, photos: [] };
         this.filterOptionsSelected = { tripType: this.state.tripType, team: this.state.team }
         this.handleTripTypeChange = this.handleTripTypeChange.bind(this);
         this.handleTeamChange = this.handleTeamChange.bind(this);
         this.updateStateFromFilter = this.updateStateFromFilter.bind(this);
         this.filterOptions = { tripTypeOptions: [], teamOptions: [] };
-        this.photos = [];
-        this.columns = 4;
-        this.rows = 0;
-        this.pageSize = 12;
     }
 
     handleTripTypeChange(event: ChangeEvent<HTMLSelectElement>) {
@@ -61,49 +55,20 @@ class Gallery extends React.Component<IGalleryProps, IGalleryState> {
         console.log('Selected Trip Type is: ' + this.state.tripType.text + ' (' + this.state.tripType.value + ')');
         console.log('Selected Team is: ' + this.state.team.text + ' (' + this.state.team.value + ')');
         this.setState({ isLoading: true });
-        let data = new Data();
-        this.getPhotos();
+        await this.getPhotos();
         this.filterOptionsSelected = { tripType: this.state.tripType, team: this.state.team }
-        this.getPageOfPhotos(0);
         this.setState({ selectedPage: 0, isLoading: false });
     }
 
-    getPageOfPhotos(pageNumber: number) {
-
-        const startIndex = pageNumber * this.pageSize;
-        let pageOfPhotos: IPhoto[] = [];
-
-        for(let i=startIndex; i < this.photos.length && i - startIndex < this.pageSize; i++) {
-            pageOfPhotos.push(this.photos[i]);
-        }
-
-        this.rows = Math.ceil((pageOfPhotos.length / this.columns));
-        let row;
-        let grid: number[][] = [];
-        for(var i=0;i<this.rows;i++){
-          row=[];
-          for(var j=0;j<this.columns;j++){
-            let index = this.columns * i + j + (pageNumber * this.pageSize);
-            row.push(index)
-          }
-          grid.push(row);
-        }
-        console.log('grid for # ' + pageNumber, grid);
-        this.setState({ photoGrid: grid });
-    }
-
     handlePageClick = (data: { selected: number }) => {
-        
         this.setState({ selectedPage: data.selected })
-        this.getPageOfPhotos(data.selected);
     };
 
     async getPhotos() {
         let data = new Data();
-        this.photos = await data.GetPhotos(this.state.tripType.value, this.state.team.value);
-        const totalPages = Math.ceil(this.photos.length/this.pageSize);
-        console.log('totalPages', totalPages);
-        this.setState({ pageCount: totalPages });
+        const photos = await data.GetPhotos(this.state.tripType.value, this.state.team.value);
+        const totalPages = Math.ceil(photos.length/this.pageSize);
+        this.setState({ pageCount: totalPages, photos: photos });
     }
 
     async componentDidMount() {
@@ -131,38 +96,33 @@ class Gallery extends React.Component<IGalleryProps, IGalleryState> {
 
                     <div className="container">
 
-                        <p className="w-100 text-center mb-3">{ this.photos.length } photos of type <em>{ this.filterOptionsSelected.tripType.text }</em> and team <em>{ this.filterOptionsSelected.team.text }</em>.</p>
-                        
-                            <ReactPaginate
-                                pageCount={this.state.pageCount}
-                                pageRangeDisplayed={5}
-                                marginPagesDisplayed={3}
-                                previousLabel={'<<'}
-                                nextLabel={'>>'}
-                                breakLabel={'...'}
-                                breakClassName={'page-item'}
-                                breakLinkClassName={'page-link'}
-                                pageClassName={'page-item'}
-                                pageLinkClassName={'page-link'}
-                                previousClassName={'page-item'}
-                                previousLinkClassName={'page-link rounded-left'}
-                                nextClassName={'page-item rounded-right'}
-                                nextLinkClassName={'page-link'}
-                                disabledClassName={'disabled'}
-                                onPageChange={this.handlePageClick}
-                                initialPage={0}
-                                containerClassName={'pagination pagination-sm justify-content-center mb-4'}
-                                activeClassName={'active'}
-                                forcePage={this.state.selectedPage}
-                            />
+                        <FilterMessage photosCount={this.state.photos.length} tripTypeName={ this.filterOptionsSelected.tripType.text } teamName={ this.filterOptionsSelected.team.text } />
 
-                        {
-                            this.state.photoGrid.map((row, index) => (
-                            <div key={`row${row[0]}`} className="row justify-content-center mb-4">
-                                {row.map((photoIndex) => photoIndex < this.photos.length ? <Photo photo={this.photos[photoIndex]} key={photoIndex} /> : <div key={photoIndex} className="col-6 col-sm-6 col-md-3 col-lg-2 mb-3">&#160;</div>)}
-                            </div>
-                            ))
-                        }
+                        <ReactPaginate
+                            pageCount={this.state.pageCount}
+                            pageRangeDisplayed={5}
+                            marginPagesDisplayed={3}
+                            previousLabel={'<<'}
+                            nextLabel={'>>'}
+                            breakLabel={'...'}
+                            breakClassName={'page-item'}
+                            breakLinkClassName={'page-link'}
+                            pageClassName={'page-item'}
+                            pageLinkClassName={'page-link'}
+                            previousClassName={'page-item'}
+                            previousLinkClassName={'page-link rounded-left'}
+                            nextClassName={'page-item rounded-right'}
+                            nextLinkClassName={'page-link'}
+                            disabledClassName={'disabled'}
+                            onPageChange={this.handlePageClick}
+                            initialPage={0}
+                            containerClassName={'pagination pagination-sm justify-content-center mb-4'}
+                            activeClassName={'active'}
+                            forcePage={this.state.selectedPage}
+                        />
+
+                        <PhotoGrid page={this.state.selectedPage} pageSize={this.pageSize} photos={this.state.photos} columns={4} />
+
                     </div>
                 </>
             );
---
+++ src/components/PhotoGrid.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import Photo from './Photo';
+
+import { IPhoto } from '../interfaces/IPhoto';
+
+interface IPhotoGridProps {
+    page: number;
+    pageSize: number;
+    columns: number;
+    photos: IPhoto[];
+}
+
+interface IPhotoGridState {
+    page: number;
+    photoGrid: number[][];
+}
+
+class PhotoGrid extends React.Component<IPhotoGridProps, IPhotoGridState> {
+
+    constructor(props: IPhotoGridProps) {
+        super(props);
+        this.state = { page: props.page, photoGrid: [] };
+    }
+
+    componentDidUpdate(prevProps: IPhotoGridProps) {
+        if ( prevProps.page !== this.props.page ){
+            this.getPageOfPhotos(this.props.page);
+        }
+    }
+
+    getPageOfPhotos(pageNumber: number) {
+
+        const startIndex = pageNumber * this.props.pageSize;
+        let pageOfPhotos: IPhoto[] = [];
+
+        for(let i=startIndex; i < this.props.photos.length && i - startIndex < this.props.pageSize; i++) {
+            pageOfPhotos.push(this.props.photos[i]);
+        }
+
+        let rows: number = Math.ceil((pageOfPhotos.length / this.props.columns));
+        let row;
+        let grid: number[][] = [];
+        for (let i=0; i < rows; i++) {
+          row=[];
+          for (let j=0; j < this.props.columns; j++){
+            let index = this.props.columns * i + j + (pageNumber * this.props.pageSize);
+            row.push(index)
+          }
+          grid.push(row);
+        }
+        
+        this.setState({ photoGrid: grid });
+    }
+
+    async componentDidMount() {
+        this.getPageOfPhotos(this.props.page);
+    }
+
+    public render() {
+        if (this.state.photoGrid.length > 0) {
+            return (
+                <>
+                    {
+                        this.state.photoGrid.map((row, index) => (
+                            <div key={`row${row[0]}`} className="row justify-content-center mb-4">
+                                {row.map((photoIndex) => photoIndex < this.props.photos.length ? <Photo photo={this.props.photos[photoIndex]} key={photoIndex} /> : <div key={photoIndex} className="col-6 col-sm-6 col-md-3 col-lg-2 mb-3">&#160;</div>)}
+                            </div>
+                        ))
+                    }
+                </>
+            );
+        } else {
+            return (<div className="row"><h5 className="mx-auto">No photos</h5></div>);
+        }
+    }
+}
+
+export default PhotoGrid;
