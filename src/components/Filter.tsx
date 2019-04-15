6bc31298f044fb5337e4a3705db3c5807769e957
Luke Jennings <lukeajennings@protonmail.com>
2019-04-14 17:28:22 -0700

    Fixed responsive display of filter labels in Chrome on Mac OS and updated colors of paging and buttons to match Seattle ICO style.

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -27,15 +27,15 @@ class Filter extends React.Component<IFilterProps, IFilterState> {
     
         <form className="form-inline justify-content-center" onSubmit={this.handleSubmit}>
   
-          <div className="form-group col-12 mb-1 col-sm-6 px-sm-0 col-md-4 col-lg-3 col-xl-2">
+          <div className="form-group text-left col-12 mb-1 col-sm-6 px-sm-2 col-md-4 col-lg-3 pl-xl-4 pr-xl-3">
               <Select labelText="Trip Type" selectName="tripTypes" selectedValue={this.props.values.tripType.value} options={this.props.options.tripTypeOptions} onChange={this.props.onTripTypeChange}  />
           </div>
 
-          <div className="form-group col-12 mb-3 col-sm-4 pt-sm-2 px-sm-0 col-md-3 col-lg-2 ml-xl-3 pl-xl-3">
+          <div className="form-group text-left col-12 mb-3 col-sm-4 pt-sm-2 px-sm-2 col-md-3 col-lg-2 mr-lg-3 mx-xl-0 pl-xl-0 pr-xl-5">
               <Select labelText="Teams" selectName="teams" selectedValue={this.props.values.team.value} options={this.props.options.teamOptions} onChange={this.props.onTeamChange}  />
           </div>
   
-          <div className="form-group col-12 mb-0 col-sm-2 pt-sm-2 px-sm-0 col-lg-1">
+          <div className="form-group text-left col-12 mb-3 mb-sm-0 col-sm-2 pt-sm-2 px-sm-0 col-lg-1">
               <button id="btnSubmit" type="submit" className="btn btn-primary">Search</button>
           </div>
   
--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -11,7 +11,7 @@ class Photo extends React.Component<IPhotoProps> {
     public render() {
         return (
             <>
-                <div className="col-6 col-sm-6 col-md-3 col-lg-2 mb-3">
+                <div className="photo-thumbnail col-6 col-sm-6 col-md-3 col-lg-2 mb-3">
                     <img src={ 'https://volunteers.seattleico.org:443' + this.props.photo.image } alt={ this.props.photo.caption } title={ this.props.photo.caption } width={ this.props.photo.widthThumb } height={ this.props.photo.heightThumb } style={{ display: 'block' }} className="rounded mx-auto photo-thumbnail" />
                     <p className="small mb-0 text-center"><strong>{ moment(this.props.photo.date).format('ddd., MMM. D, Y') }</strong></p>
                     <p className="small mb-0 text-center">[{ this.props.photo.team }] { this.props.photo.destination }</p>
