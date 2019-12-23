bfb8c9d0a5563a1276bfaf7ac3b24c29103b6b12
Luke Jennings <lukeajennings@protonmail.com>
2019-12-23 11:31:22 -0800

    Convert all React Class Components to Functional Components that do not manage state or lifecycle.

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,50 +1,42 @@
-import React, { FormEvent } from 'react';
+import React from 'react';
 
 import { IFilterProps } from '../interfaces/IFilterProps';
-import { IFilterState } from '../interfaces/IFilterState';
 
 import Select from './Select';
 
-class Filter extends React.Component<IFilterProps, IFilterState> {
+const Filter = (props: IFilterProps) => {
 
-  constructor(props: IFilterProps) {
-    super(props);
-  }
+  if (props.options.tripTypeOptions.length > 0 && props.options.teamOptions.length > 0) {
 
-  public render() {
+    return (
     
-    if (this.props.options.tripTypeOptions.length > 0 && this.props.options.teamOptions.length > 0) {
-
-      return (
+        <div className="col-11 col-sm-12 mx-auto">
       
-          <div className="col-11 col-sm-12 mx-auto">
-        
-            <form className="form-inline justify-content-center" onSubmit={this.props.onSubmit}>
-      
-              <div className="form-group text-left col-12 mb-1 col-sm-6 px-sm-2 col-md-4 col-lg-3 pl-xl-4 pr-xl-3">
-                  <Select labelText="Trip Type" selectName="tripTypes" selectedValue={this.props.values.tripType.value} options={this.props.options.tripTypeOptions} onChange={this.props.onTripTypeChange}  />
-              </div>
+          <form className="form-inline justify-content-center" onSubmit={props.onSubmit}>
+    
+            <div className="form-group text-left col-12 mb-1 col-sm-6 px-sm-2 col-md-4 col-lg-3 pl-xl-4 pr-xl-3">
+                <Select labelText="Trip Type" selectName="tripTypes" selectedValue={props.values.tripType.value} options={props.options.tripTypeOptions} onChange={props.onTripTypeChange}  />
+            </div>
 
-              <div className="form-group text-left col-12 mb-3 col-sm-4 pt-sm-2 px-sm-2 col-md-3 col-lg-2 mr-lg-3 mx-xl-0 pl-xl-0 pr-xl-5">
-                  <Select labelText="Teams" selectName="teams" selectedValue={this.props.values.team.value} options={this.props.options.teamOptions} onChange={this.props.onTeamChange}  />
-              </div>
-      
-              <div className="form-group text-left col-12 mb-3 mb-sm-0 col-sm-2 pt-sm-2 px-sm-0 col-lg-1">
-                  <button id="btnSubmit" type="submit" className="btn btn-primary">Search</button>
-              </div>
-      
-            </form>
-      
-          </div>
-      );
+            <div className="form-group text-left col-12 mb-3 col-sm-4 pt-sm-2 px-sm-2 col-md-3 col-lg-2 mr-lg-3 mx-xl-0 pl-xl-0 pr-xl-5">
+                <Select labelText="Teams" selectName="teams" selectedValue={props.values.team.value} options={props.options.teamOptions} onChange={props.onTeamChange}  />
+            </div>
+    
+            <div className="form-group text-left col-12 mb-3 mb-sm-0 col-sm-2 pt-sm-2 px-sm-0 col-lg-1">
+                <button id="btnSubmit" type="submit" className="btn btn-primary">Search</button>
+            </div>
+    
+          </form>
+    
+        </div>
+    );
 
-    } else {
+  } else {
 
-      return (
-          <h2 className="mx-auto">Loading Filters...</h2>
-      );
+    return (
+        <h2 className="mx-auto">Loading Filters...</h2>
+    );
 
-    }
   }
 }
 
--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -1,27 +1,21 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 
 import { IThumbnailProps } from '../interfaces/IThumbnailProps';
 import moment from 'moment';
 
-class Thumbnail extends React.Component<IThumbnailProps> {
-    
-    public constructor(props: IThumbnailProps) {
-      super(props);
-    }
+const Thumbnail: FunctionComponent<IThumbnailProps> = ({photo, onClick}) => {
 
-    public render() {
-        return (
-            <>
-                <div className="photo-thumbnail col-6 col-sm-6 col-md-3 col-lg-2 mb-3">
-                    <a onClick={this.props.onClick} id={this.props.photo.id.toString()}>
-                        <img src={ process.env.REACT_APP_PROTOCOL_HOSTNAME + this.props.photo.image } alt={ this.props.photo.caption } title={ this.props.photo.caption } width={ this.props.photo.widthThumb } height={ this.props.photo.heightThumb } style={{ display: 'block' }} className="rounded mx-auto photo-thumbnail" />
-                    </a>
-                    <p className="small mb-0 text-center"><strong>{ moment(this.props.photo.date).format('ddd., MMM. D, Y') }</strong></p>
-                    <p className="small mb-0 text-center">[{ this.props.photo.team }] { this.props.photo.destination }</p>
-                </div>
-            </>
-        );
-    }
+    return (
+        <>
+            <div className="photo-thumbnail col-6 col-sm-6 col-md-3 col-lg-2 mb-3">
+                <a onClick={onClick} id={photo.id.toString()}>
+                    <img src={ process.env.REACT_APP_PROTOCOL_HOSTNAME + photo.image } alt={ photo.caption } title={ photo.caption } width={ photo.widthThumb } height={ photo.heightThumb } style={{ display: 'block' }} className="rounded mx-auto photo-thumbnail" />
+                </a>
+                <p className="small mb-0 text-center"><strong>{ moment(photo.date).format('ddd., MMM. D, Y') }</strong></p>
+                <p className="small mb-0 text-center">[{ photo.team }] { photo.destination }</p>
+            </div>
+        </>
+    );
 }
 
 export default Thumbnail;
