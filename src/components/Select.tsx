50f552aaab42a4f0ff01d21e7de522dd0a3f25b6
Luke Jennings <lukeajennings@protonmail.com>
2019-12-23 14:27:25 -0800

    Convert ThumbnailGrid to a function component and use hooks for state and lifecycle.

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -5,14 +5,14 @@ import { ISelectProps } from '../interfaces/ISelectProps';
 const Select = (props: ISelectProps) => {
 
     return (
-        <>
+        <React.Fragment>
             <label htmlFor={props.selectName} className="small mb-1">{props.labelText}</label>
             <select id={props.selectName} name={props.selectName} className="form-control" value={props.selectedValue} onChange={props.onChange}>
                 {props.options.map(o => (
                     <option key={o.value} value={o.value}>{o.text}</option>
                 ))}
             </select>
-        </>
+        </React.Fragment>
     );
 }
 
--- src/components/Thumbnail.tsx
+++ src/components/Thumbnail.tsx
@@ -3,10 +3,16 @@ import React, { FunctionComponent } from 'react';
 import { IThumbnailProps } from '../interfaces/IThumbnailProps';
 import moment from 'moment';
 
-const Thumbnail: FunctionComponent<IThumbnailProps> = ({photo, onClick}) => {
+/**
+ * @description Return a single thumbnail image.
+ *
+ * @param {IThumbnailProps} {photo, onClick}
+ * @returns
+ */
+const Thumbnail: FunctionComponent<IThumbnailProps> = ({photo, onClick}): JSX.Element => {
 
     return (
-        <>
+        <React.Fragment>
             <div className="photo-thumbnail col-6 col-sm-6 col-md-3 col-lg-2 mb-3">
                 <a onClick={onClick} id={photo.id.toString()}>
                     <img src={ process.env.REACT_APP_PROTOCOL_HOSTNAME + photo.image } alt={ photo.caption } title={ photo.caption } width={ photo.widthThumb } height={ photo.heightThumb } style={{ display: 'block' }} className="rounded mx-auto photo-thumbnail" />
@@ -14,7 +20,7 @@ const Thumbnail: FunctionComponent<IThumbnailProps> = ({photo, onClick}) => {
                 <p className="small mb-0 text-center"><strong>{ moment(photo.date).format('ddd., MMM. D, Y') }</strong></p>
                 <p className="small mb-0 text-center">[{ photo.team }] { photo.destination }</p>
             </div>
-        </>
+        </React.Fragment>
     );
 }
 
